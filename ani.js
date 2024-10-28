4ac028e491adcafce84a431c828b5068f962ae91
Jiten <cjiten15@gmail.com>
2024-10-28 16:59:11 +0530

    Updated

--- ani.js
+++ ani.js
@@ -1,46 +1,46 @@
 gsap.to(".selector1", { // selector text, Array, or object
-    x: 25,
-    duration: 3,
-    delay: 1,
-    yoyo: true,
-    repeat: -1, // number of repeats (-1 for infinite)
-    repeatDelay: 1, // seconds between repeats
-    repeatRefresh: true,
-    ease: "power2.inOut",
-  });
-  
+  x: 25,
+  duration: 3,
+  delay: 1,
+  yoyo: true,
+  repeat: -1, // number of repeats (-1 for infinite)
+  repeatDelay: 1, // seconds between repeats
+  repeatRefresh: true,
+  ease: "power2.inOut",
+});
+
 gsap.to(".selector2", { // selector text, Array, or object
-    y: 22,
-    duration: 3,
-    delay: 1,
-    yoyo: true,
-    repeat: -1, // number of repeats (-1 for infinite)
-    repeatDelay: 1, // seconds between repeats
-    repeatRefresh: true,
-    ease: "power2.inOut",
-  });
+  y: 22,
+  duration: 3,
+  delay: 1,
+  yoyo: true,
+  repeat: -1, // number of repeats (-1 for infinite)
+  repeatDelay: 1, // seconds between repeats
+  repeatRefresh: true,
+  ease: "power2.inOut",
+});
 
 gsap.to(".selector3", { // selector text, Array, or object
-    y: -22,
-    duration: 3,
-    delay: 1,
-    yoyo: true,
-    repeat: -1, // number of repeats (-1 for infinite)
-    repeatDelay: 1, // seconds between repeats
-    repeatRefresh: true,
-    ease: "power2.inOut",
-  });
-  
+  y: -22,
+  duration: 3,
+  delay: 1,
+  yoyo: true,
+  repeat: -1, // number of repeats (-1 for infinite)
+  repeatDelay: 1, // seconds between repeats
+  repeatRefresh: true,
+  ease: "power2.inOut",
+});
+
 gsap.to(".selector4", { // selector text, Array, or object
-    x: -25,
-    duration: 3,
-    delay: 1,
-    yoyo: true,
-    repeat: -1, // number of repeats (-1 for infinite)
-    repeatDelay: 1, // seconds between repeats
-    repeatRefresh: true,
-    ease: "power2.inOut",
-  });
+  x: -25,
+  duration: 3,
+  delay: 1,
+  yoyo: true,
+  repeat: -1, // number of repeats (-1 for infinite)
+  repeatDelay: 1, // seconds between repeats
+  repeatRefresh: true,
+  ease: "power2.inOut",
+});
 
 
 const images = document.querySelectorAll(".image"); // Selects all elements with class "image"
